9de0b106e095e4ac1b70db1e943ab3e16665dade
Shivam Singh <shivamsinghbsr0@gmail.com>
2025-11-05 23:49:19 +0530

    schema setup

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 import express from "express";
-
+import authController from "./controllers/auth.controller.js";
 
 const app = express();
 
+app.use(express.json());
+app.use("/api/auth", authController);
 
 
 export default app;
