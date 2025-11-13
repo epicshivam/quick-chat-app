d8864edec057db51c433dd44a777fc3a7837482b
Shivam Singh <shivamsinghbsr0@gmail.com>
2025-11-13 23:26:30 +0530

    /get-all-chat for current userwhom he has started with

--- server/controllers/chat.controller.js
+++ server/controllers/chat.controller.js
@@ -1,7 +1,7 @@
 import express from "express";
 const router = express.Router();
 import mongoose from "mongoose";
-import authMiddleware from "../middlewares/authMiddleware";
+import authMiddleware from "../middlewares/authMiddleware.js";
 import Chat from "../models/chat.model.js";
 
 
@@ -15,6 +15,25 @@ router.post("/create-new-chat", authMiddleware, async (req, res)=> {
             message:`Chat created successfully`,
             success:true,
             data:savedChat
+        })
+        
+    } catch (error) {
+        res.status(400).json({
+            message:error.message,
+            success:false
+        })
+    }
+})
+
+router.get("/get-all-chat", authMiddleware, async (req, res)=> {
+    try {
+
+        const allChat = await Chat.find({members : {$in : req.user}});
+        
+        res.status(201).json({
+            message:`Chats found successfully`,
+            success:true,
+            data:allChat
         })
         
     } catch (error) {
