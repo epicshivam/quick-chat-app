332d806edded4c4ec2b66d35046ee3324e869588
Shivam Singh <shivamsinghbsr0@gmail.com>
2025-12-11 02:10:53 +0530

    Login page Draft

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {Link} from "react-router-dom";
 
 const Login = () => {
+
+  const [user, setUser] = useState({email : "", password : ""});
+
+  function formSubmitHandler(event) {
+    event.preventDefault();
+    console.log(user);
+  }
+
   return (
-    <div>
-      
+    <div className='h-screen w-full flex justify-center items-center bg-slate-800'>
+      <div className='w-sm bg-zinc-500 rounded-xl text-center py-4 px-3'>
+        <h1 className='text-semibold text-center text-2xl m-5 '>Login Page</h1>
+        <form className='mx-5 flex flex-col' onSubmit={formSubmitHandler}>
+          <input className='bg-gray-100 px-4 py-2 text-l my-2 rounded-sm' type="email" name="email" placeholder='Email' value={user.email} onChange={(e) => setUser({...user , email : e.target.value})}/>
+          <input className='bg-gray-100 px-4 py-2 text-l my-2 rounded-sm' type="password" name="password" placeholder='Password' value={user.password} onChange={(e)=>setUser({...user, password : e.target.value})}/>
+          <button className='text-xl bg-blue-200 my-5 p-2 border rounded-3xl focus:outline-none focus:ring-0 focus:ring-transparent' type="submit">Login</button>
+        </form>
+        <h5>New comer ? <Link to="/signup" className='text-blue-600 underline'>Signup Here</Link></h5>
+      </div>
     </div>
   )
 }
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,19 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {Link} from "react-router-dom";
 
 const Signup = () => {
+
+  const [user, setUser] = useState({firstName : "", lastName : "", email : "", password : ""});
+
+  function formSubmitHandler(event) {
+    event.preventDefault();
+    console.log(user);
+  }
+
   return (
     <div className='h-screen w-full flex justify-center items-center bg-slate-800'>
-      <div className='w-auto bg-zinc-500 rounded-xl'>
+      <div className='w-auto bg-zinc-500 rounded-xl text-center py-4 px-3'>
         <h1 className='text-semibold text-center text-2xl m-5 '>SignUp Page</h1>
-        <form className='mx-5 flex flex-col'>
+        <form className='mx-5 flex flex-col' onSubmit={formSubmitHandler}>
           <div className='flex my-2'>
-            <input className='bg-gray-100 px-4 py-2 text-l mx-1 rounded-sm' type="text" name="first-name" placeholder='First Name'/>
-            <input className='bg-gray-100 px-4 py-2 text-l rounded-sm' type="text" name="last-name" placeholder='Last Name' />
+            <input className='bg-gray-100 px-4 py-2 text-l mx-1 rounded-sm' type="text" name="first-name" placeholder='First Name' value={user.firstName} onChange={(e) => setUser({...user , firstName : e.target.value})}/>
+            <input className='bg-gray-100 px-4 py-2 text-l rounded-sm' type="text" name="last-name" placeholder='Last Name' value={user.lastName} onChange={(e) => setUser({...user, lastName : e.target.value})}/>
           </div>
-          <input className='bg-gray-100 px-4 py-2 text-l my-2 rounded-sm' type="email" name="email" placeholder='Email' />
-          <input className='bg-gray-100 px-4 py-2 text-l my-2 rounded-sm' type="password" name="password" placeholder='Password' />
+          <input className='bg-gray-100 px-4 py-2 text-l my-2 rounded-sm' type="email" name="email" placeholder='Email' value={user.email} onChange={(e) => setUser({...user , email : e.target.value})}/>
+          <input className='bg-gray-100 px-4 py-2 text-l my-2 rounded-sm' type="password" name="password" placeholder='Password' value={user.password} onChange={(e)=>setUser({...user, password : e.target.value})}/>
           <button className='text-xl bg-blue-200 my-5 p-2 border rounded-3xl focus:outline-none focus:ring-0 focus:ring-transparent' type="submit">Signup</button>
         </form>
+        <h5>Already have an account? <Link to="/login" className='text-blue-600 underline'>Login Here</Link></h5>
       </div>
     </div>
   )
