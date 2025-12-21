93a7d27e113b2ad80765c2f8f005000ff7b99512
Shivam Singh <shivamsinghbsr0@gmail.com>
2025-12-21 13:17:56 +0530

    Route Protection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home.jsx";
 import Signup from "./pages/Signup.jsx";
 import Login from "./pages/Login.jsx";
 import {Toaster} from "react-hot-toast";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
     <Toaster position="top-center" reverseOrder={false}/>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
         </Routes>
